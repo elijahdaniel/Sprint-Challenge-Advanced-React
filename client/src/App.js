2dc74e4b544a46ec0467fe562e2bfc38f7ae460d
Elijah Paminsan <e.paminsan@gmail.com>
2019-09-13 19:13:41 -0700

    fetched data from local api

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    userCard: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:5000/api/players')
+      .then(res => res.json()) // format as json object
+      .then(data => this.setState({ userCard: data }))
+      .catch(err => console.error(err))
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.name}</p>
+      </div>
+    )
+  }
 }
-
-export default App;
